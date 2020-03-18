85b0c75f4c5b453fe6af8a084ae77e27a55e7a02
iOS曹 <clh2046@163.com>
2020-03-18 15:15:11 +0800

    0.1.2 ScrollOptionsMenuView 支持水平和竖直方向滚动时切换

--- CLHComponents/Classes/ScrollOptionsMenu/ScrollOptionsMenuView.m
+++ CLHComponents/Classes/ScrollOptionsMenu/ScrollOptionsMenuView.m
@@ -45,7 +45,7 @@
     self.titleBtn.selected = model.isSelected;
 }
 
-- (void)updateTitleColor:(UIColor *_Nullable)normalColor
+- (void)setNormalTitleColor:(UIColor *_Nullable)normalColor
               normalFont:(UIFont *_Nullable)normalFont
            selectedColor:(UIColor *_Nullable)selectedColor
             selectedFont:(UIFont *_Nullable)selectedFont {
@@ -82,7 +82,10 @@
     CGFloat _animateDuration;
     CGFloat _minimumInteritemSpacing;
     UIEdgeInsets _contentEdgeInsets;
-    
+    NSIndexPath *_currentSelectedIndexPath;
+    CGFloat _startContentOffset;
+    BOOL _isTapSelected;
+    ScrollDirection _scrollDirection;
 }
 
 - (UICollectionView *)collectionView {
@@ -103,58 +106,69 @@
 
 - (UIImageView *)highlightLine {
     if (_highlightLine == nil) {
-        _highlightLine = [[UIImageView alloc] initWithFrame:CGRectZero];
+        _highlightLine = [[UIImageView alloc] initWithFrame:CGRectMake(0, self.frame.size.height - _lineSize.height - 2, _lineSize.width, _lineSize.height)];
         _highlightLine.backgroundColor = [UIColor colorWithRed:255.f/255.f green:83.f/255.f blue:0.f/255.f alpha:1];
     }
     return _highlightLine;
 }
 - (instancetype)initWithFrame:(CGRect)frame {
+    self = [self initWithFrame:frame scrollDirection:ScrollDirectionHorizontal];
+    if (self){
+        
+    }
+    return self;
+}
+- (instancetype)initWithFrame:(CGRect)frame scrollDirection:(ScrollDirection)scrollDirection {
     self = [super initWithFrame:frame];
     if (self) {
+        _scrollDirection = scrollDirection;
         [self setDefoutWithFrame:frame];
     }
     return self;
 }
-
 - (void)setDefoutWithFrame:(CGRect)frame {
     _animateDuration = 0.23;
+    _isTapSelected = NO;
     _normalFont = [UIFont fontWithName:@"PingFangSC-Medium" size:15];
-    _selectedFont = [UIFont fontWithName:@"PingFangSC-Medium" size:20];
+    _selectedFont = [UIFont fontWithName:@"PingFangSC-Medium" size:15];
     [self setLineSize:CGSizeMake(30, 2)];
     _normalColor = [UIColor colorWithRed:51.f/255.f green:51.f/255.f blue:51.f/255.f alpha:1];
     _selectedColor = [UIColor colorWithRed:255.f/255.f green:83.f/255.f blue:0.f/255.f alpha:1];
     _contentEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 10);
     _minimumInteritemSpacing = 10;
+    _currentSelectedIndexPath = [NSIndexPath indexPathForRow:0 inSection:0];
     self.collectionView.frame = CGRectMake(_contentEdgeInsets.left, _contentEdgeInsets.top, CGRectGetWidth(self.frame) - 2*_contentEdgeInsets.left, CGRectGetHeight(self.frame) - (_contentEdgeInsets.top + _contentEdgeInsets.bottom));
     [self addSubview:self.collectionView];
     [self.collectionView addSubview:self.highlightLine];
 }
 - (void)updateSelectedIndex:(NSInteger)selectedIndex {
     NSIndexPath *indexPath = [NSIndexPath indexPathForRow:selectedIndex inSection:0];
-    [self scrollToSelectedCellWith:indexPath];
+    _currentSelectedIndexPath = indexPath;
+    [self scrollToSelectedCellWithSelectedIndexPath:_currentSelectedIndexPath];
     
     for (OptionsModel *model in self.dataArray) {
         model.isSelected = NO;
     }
     
-    OptionsModel *selectedMOdel = [self.dataArray objectAtIndex:indexPath.row];
+    OptionsModel *selectedMOdel = [self.dataArray objectAtIndex:_currentSelectedIndexPath.row];
     selectedMOdel.isSelected = YES;
     [self.collectionView reloadData];
     [self.collectionView setNeedsLayout];
     [UIView animateWithDuration:_animateDuration animations:^{
-        [self setLineFrameWithIndexPath:indexPath];
+        [self setLineSize:self->_lineSize];
+        self.highlightLine.center = [self getLineFrameWithIndexPath:self->_currentSelectedIndexPath];
     }];
 }
 - (void)updateDataArray:(NSArray<NSString *>*)dataArray {
     self.dataArray = [self getModels:dataArray];
     [self.collectionView reloadData];
     [self setLineSize:_lineSize];
-    [self setLineFrameWithIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+    self.highlightLine.center = [self getLineFrameWithIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
 }
-- (void)updateTitleColor:(UIColor *_Nullable)normalColor
-              normalFont:(UIFont *_Nullable)normalFont
-           selectedColor:(UIColor *_Nullable)selectedColor
-            selectedFont:(UIFont *_Nullable)selectedFont {
+- (void)setNormalTitleColor:(UIColor *_Nullable)normalColor
+                 normalFont:(UIFont *_Nullable)normalFont
+              selectedColor:(UIColor *_Nullable)selectedColor
+               selectedFont:(UIFont *_Nullable)selectedFont {
     
     _normalColor = normalColor?:_normalColor;
     _normalFont = normalFont?:_normalFont;
@@ -164,7 +178,7 @@
 }
 #pragma mark - UICollectionViewDelegate
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-    
+    _isTapSelected = YES;
     [self updateSelectedIndex:indexPath.row];
     
     if([self.delegate respondsToSelector:@selector(didSelectedItemWithIndex:)]) {
@@ -183,7 +197,7 @@
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     OptionsCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cell_Identifier forIndexPath:indexPath];
     [cell updateWithModel:[self.dataArray objectAtIndex:indexPath.row]];
-    [cell updateTitleColor:_normalColor normalFont:_normalFont selectedColor:_selectedColor selectedFont:_selectedFont];
+    [cell setNormalTitleColor:_normalColor normalFont:_normalFont selectedColor:_selectedColor selectedFont:_selectedFont];
     return cell;
 }
 
@@ -195,36 +209,110 @@
     } else {
         itemSize = [model.title sizeWithAttributes:@{NSFontAttributeName:_normalFont}];
     }
-    return itemSize;
+    return CGSizeMake(itemSize.width, collectionView.bounds.size.height);
 }
 
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
     return _minimumInteritemSpacing;
 }
 
-- (void)scrollToSelectedCellWith:(NSIndexPath *)selectedIndexPath {
+- (void)scrollToSelectedCellWithSelectedIndexPath:(NSIndexPath *)selectedIndexPath {
     
     NSArray *visibleIndexArray = [[self.collectionView indexPathsForVisibleItems] sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
         return [obj1 compare:obj2];
     }];
     
     if ([selectedIndexPath isEqual:[visibleIndexArray lastObject]]) {
-         NSLog(@"点击的是正在显示的cell 数组右边的cell");
-        if (selectedIndexPath.row + 1 <= self.dataArray.count - 1) {
-            [self.collectionView selectItemAtIndexPath:[NSIndexPath indexPathForRow:selectedIndexPath.row + 1 inSection:0] animated:YES scrollPosition:UICollectionViewScrollPositionRight];
-        } else {//点击的事数据源最后一个数据对应的cell
-            [self.collectionView selectItemAtIndexPath:[NSIndexPath indexPathForRow:self.dataArray.count - 1 inSection:0] animated:YES scrollPosition:UICollectionViewScrollPositionRight];
-        }
+        [self.collectionView scrollToItemAtIndexPath:[self getNextInexPathWithCurrendIndexPath:selectedIndexPath]  atScrollPosition:UICollectionViewScrollPositionRight animated:YES];
     } else if ([selectedIndexPath isEqual:[visibleIndexArray firstObject]]) {
-        NSLog(@"点击的是正在显示的cell 数组左边的cell");
-        if (selectedIndexPath.row - 1 >= 0) {
-            [self.collectionView selectItemAtIndexPath:[NSIndexPath indexPathForRow:selectedIndexPath.row - 1 inSection:0] animated:YES scrollPosition:UICollectionViewScrollPositionLeft];
-        } else {//点击的事数据源第一个数据对应的cell
-            [self.collectionView selectItemAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0] animated:YES scrollPosition:UICollectionViewScrollPositionLeft];
-        }
+        [self.collectionView scrollToItemAtIndexPath:[self getPreviousInexPathWithCurrendIndexPath:selectedIndexPath]  atScrollPosition:UICollectionViewScrollPositionLeft animated:YES];
     }
     if (![visibleIndexArray containsObject:selectedIndexPath]) {
         [self.collectionView selectItemAtIndexPath:selectedIndexPath animated:YES scrollPosition:UICollectionViewScrollPositionCenteredHorizontally];
+    }
+}
+- (void)willBeginDragging:(UIScrollView *)scrollView {
+    _isTapSelected = NO;
+    switch (_scrollDirection) {
+        case ScrollDirectionVertical:
+            _startContentOffset = scrollView.contentOffset.y;
+            break;
+        default:
+            _startContentOffset = scrollView.contentOffset.x;
+            break;
+    }
+}
+
+- (void)didScroll:(UIScrollView *)scrollView {
+    if (_isTapSelected) { return; }
+    if (_scrollDirection == ScrollDirectionVertical) {
+        CGFloat scrollHeight = fabs(scrollView.contentOffset.y - _startContentOffset);
+        if(scrollView.contentOffset.y - _startContentOffset > 0) {
+            NSIndexPath *nextIndexPath = [self getNextInexPathWithCurrendIndexPath:_currentSelectedIndexPath];
+            if(![nextIndexPath isEqual:_currentSelectedIndexPath]) {
+                CGPoint nextLinePoint = [self getLineFrameWithIndexPath:nextIndexPath];
+                CGFloat toNextDifference = nextLinePoint.x - self.highlightLine.center.x;
+                CGFloat toNextaAddMaxWidth = toNextDifference + _lineSize.width / 2;
+                self.highlightLine.frame = CGRectMake(self.highlightLine.frame.origin.x, self.highlightLine.frame.origin.y, _lineSize.width + (scrollHeight/scrollView.bounds.size.height*toNextaAddMaxWidth), _lineSize.height);
+            }
+        } else {
+            NSIndexPath *previousIndexPath = [self getPreviousInexPathWithCurrendIndexPath:_currentSelectedIndexPath];
+            if (![previousIndexPath isEqual:_currentSelectedIndexPath]) {
+                CGFloat previousLineX= [self getLineFrameWithIndexPath:previousIndexPath].x - _lineSize.width/2;
+                CGFloat currentLineX = [self getLineFrameWithIndexPath:_currentSelectedIndexPath].x - _lineSize.width/2;
+                CGFloat toPreviousDifference = currentLineX - previousLineX;
+                
+                self.highlightLine.frame = CGRectMake(currentLineX - (scrollHeight/scrollView.bounds.size.height*toPreviousDifference), self.highlightLine.frame.origin.y, _lineSize.width + (scrollHeight/scrollView.bounds.size.height*toPreviousDifference), _lineSize.height);
+            }
+        }
+        
+    } else {
+        
+        CGFloat scrollWidth = fabs(scrollView.contentOffset.x - _startContentOffset);
+        if(scrollView.contentOffset.x - _startContentOffset > 0) {
+            NSIndexPath *nextIndexPath = [self getNextInexPathWithCurrendIndexPath:_currentSelectedIndexPath];
+            if(![nextIndexPath isEqual:_currentSelectedIndexPath]) {
+                CGPoint nextLinePoint = [self getLineFrameWithIndexPath:nextIndexPath];
+                CGFloat toNextDifference = nextLinePoint.x - self.highlightLine.center.x;
+                CGFloat toNextaAddMaxWidth = toNextDifference + _lineSize.width / 2;
+                self.highlightLine.frame = CGRectMake(self.highlightLine.frame.origin.x, self.highlightLine.frame.origin.y, _lineSize.width + (scrollWidth/scrollView.bounds.size.width*toNextaAddMaxWidth), _lineSize.height);
+            }
+        } else {
+            NSIndexPath *previousIndexPath = [self getPreviousInexPathWithCurrendIndexPath:_currentSelectedIndexPath];
+            if (![previousIndexPath isEqual:_currentSelectedIndexPath]) {
+                CGFloat previousLineX= [self getLineFrameWithIndexPath:previousIndexPath].x - _lineSize.width/2;
+                CGFloat currentLineX = [self getLineFrameWithIndexPath:_currentSelectedIndexPath].x - _lineSize.width/2;
+                CGFloat toPreviousDifference = currentLineX - previousLineX;
+                
+                self.highlightLine.frame = CGRectMake(currentLineX - (scrollWidth/scrollView.bounds.size.width*toPreviousDifference), self.highlightLine.frame.origin.y, _lineSize.width + (scrollWidth/scrollView.bounds.size.width*toPreviousDifference), _lineSize.height);
+            }
+        }
+    }
+    
+}
+
+- (void)didEndDecelerating:(UIScrollView *)scrollView {
+    if (_scrollDirection == ScrollDirectionVertical) {
+        [self updateSelectedIndex:scrollView.contentOffset.y/scrollView.bounds.size.height];
+    } else {
+       [self updateSelectedIndex:scrollView.contentOffset.x/scrollView.bounds.size.width];
+    }
+    
+}
+
+- (NSIndexPath *)getPreviousInexPathWithCurrendIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row - 1 >= 0) {
+        return [NSIndexPath indexPathForRow:indexPath.row - 1 inSection:0];
+    } else {//点击的事数据源第一个数据对应的cell
+        return [NSIndexPath indexPathForRow:0 inSection:0];
+    }
+}
+
+- (NSIndexPath *)getNextInexPathWithCurrendIndexPath:(NSIndexPath *)indexPath {
+     if (indexPath.row + 1 <= self.dataArray.count - 1) {
+        return [NSIndexPath indexPathForRow:indexPath.row + 1 inSection:0];
+    } else {//点击的事数据源第一个数据对应的cell
+        return [NSIndexPath indexPathForRow:self.dataArray.count - 1 inSection:0];
     }
 }
 
@@ -239,17 +327,16 @@
     }
     return modelArray;
 }
-- (CGPoint)setLineFrameWithIndexPath:(NSIndexPath *)indexPath {
+- (CGPoint)getLineFrameWithIndexPath:(NSIndexPath *)indexPath {
     OptionsCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:cell_Identifier forIndexPath:indexPath];
-    self.highlightLine.frame = CGRectMake(0, self.frame.size.height - _lineSize.height - 2, _lineSize.width, _lineSize.height);
     CGPoint point = CGPointMake(cell.center.x, self.highlightLine.center.y);
-    self.highlightLine.center = point;
     return point;
 }
 
 - (void)setLineSize:(CGSize)lineSize {
     _lineSize = lineSize;
-    [self setLineFrameWithIndexPath:[NSIndexPath indexPathForItem:0 inSection:0]];
+    self.highlightLine.frame = CGRectMake(self.highlightLine.frame.origin.x, self.highlightLine.frame.origin.y, _lineSize.width, _lineSize.height);
+    self.highlightLine.center = [self getLineFrameWithIndexPath:[NSIndexPath indexPathForItem:0 inSection:0]];
 }
 
 - (void)setAnimateDuration:(CGFloat)duration {
@@ -261,7 +348,7 @@
     [self.collectionView reloadData];
 }
 
-- (void)setConetentEdgeInsets:(UIEdgeInsets)edgeInsets {
+- (void)setContentEdgeInsets:(UIEdgeInsets)edgeInsets {
     _contentEdgeInsets = edgeInsets;
     self.collectionView.frame = CGRectMake(_contentEdgeInsets.left, _contentEdgeInsets.top, CGRectGetWidth(self.frame) - 2*_contentEdgeInsets.left, CGRectGetHeight(self.frame) - (_contentEdgeInsets.top + _contentEdgeInsets.bottom));
     [self addSubview:self.collectionView];
