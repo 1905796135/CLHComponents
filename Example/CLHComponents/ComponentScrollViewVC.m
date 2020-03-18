690e00ba44e1be5fdcf031a4a28efce10160deca
iOS曹 <clh2046@163.com>
2020-03-18 15:15:11 +0800

    0.1.2

--- Example/CLHComponents/ComponentScrollViewVC.m
+++ Example/CLHComponents/ComponentScrollViewVC.m
@@ -19,7 +19,7 @@
 @implementation ComponentScrollViewVC
 - (ScrollOptionsMenuView *)optionsView {
     if (!_optionsView) {
-        _optionsView = [[ScrollOptionsMenuView alloc]initWithFrame:CGRectMake(50, 135, UIScreen.width - 100, 35)];
+        _optionsView = [[ScrollOptionsMenuView alloc]initWithFrame:CGRectMake(50, 135, UIScreen.width - 100, 35) scrollDirection:ScrollDirectionVertical];
         [_optionsView updateDataArray:[self tits]];
         [_optionsView setLineSize:CGSizeMake(15, 3)];
         [_optionsView setMinimumInteritemSpacing:20];
@@ -30,8 +30,8 @@
 }
 - (UIScrollView *)mainScrollView {
     if (!_mainScrollView) {
-        _mainScrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 20, UIScreen.width, UIScreen.height)];
-        [_mainScrollView setContentSize:CGSizeMake([self tits].count*UIScreen.width, UIScreen.height - 200)];
+        _mainScrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 20, UIScreen.width, UIScreen.height - 200)];
+        [_mainScrollView setContentSize:CGSizeMake(UIScreen.width, [self tits].count*(UIScreen.height - 200))];
         _mainScrollView.delegate = self;
         _mainScrollView.pagingEnabled = YES;
     }
@@ -45,7 +45,7 @@
     [self.view addSubview:self.mainScrollView];
     
     for (int i = 0; i < [self tits].count; i++) {
-        UIView *vie = [[UIView alloc]initWithFrame:CGRectMake(i*UIScreen.width, 0, UIScreen.width, UIScreen.height - 200)];
+        UIView *vie = [[UIView alloc]initWithFrame:CGRectMake(0, i*(UIScreen.height - 200), UIScreen.width, UIScreen.height - 200)];
         vie.backgroundColor = i%2==0 ? [UIColor greenColor]:[UIColor blueColor];
         [self.mainScrollView addSubview:vie];
     }
@@ -59,15 +59,18 @@
 #pragma mark - UIScrollViewDelegate
 - (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
 //    NSLog(@"WillBeginDragging - %f  - %f",scrollView.contentOffset.x,scrollView.contentSize.width);
+    [self.optionsView willBeginDragging:scrollView];
 }
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
 //    NSLog(@"DidScroll - %f  - %f",scrollView.contentOffset.x,scrollView.contentSize.width);
+    [self.optionsView didScroll:scrollView];
 }
 - (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset {
 //    NSLog(@"WillEndDragging - %f  - %f",scrollView.contentOffset.x,scrollView.contentSize.width);
 }
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-    [self.optionsView updateSelectedIndex:scrollView.contentOffset.x/UIScreen.width];
+    [self.optionsView didEndDecelerating:scrollView];
+    
 }
 - (NSArray<NSString *>*)tits {
     return @[@"推asasasa荐",@"ddd",@"社sss会",@"科技",@"电影",@"国际新闻",@"体育新闻",@"国际体育新闻",@"aaa体育新闻"];
